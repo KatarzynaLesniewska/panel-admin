8e2f38b5c03f785b5a9760b5b37c931a5b9d5995
Kasia Leśniewska <klesniewska29@gmail.com>
2020-06-09 17:33:42 +0200

    Add show / hide JS function for menu

---
+++ src/js/script.js
@@ -0,0 +1,23 @@
+/*
+{
+  // https://kodilla.com/pl/bootcamp-module/298/262/5212#132-mobilne-menu-sterowanie-menu
+  // Show / hide menu function
+
+  function toggleMenu(visible) {
+    document.querySelector('.menu').classList.toggle('show', visible)
+  }
+
+  // po wywołaniu fn toggleMenu(true) do obiektu z kalsą menu + show
+  // po wywołaniu fn toggleMenu(false) zostanie ona usunieta
+  // jeśli użyjemy tej funkcji bez argumentu toggleMenu(), to w
+  // zależności od tego, czy klasa show już istnieje czy nie, to
+  // zostanie dodana lub usunieta
+
+  // Później wystarczy ją podczepić do wybranych elementów za pomocą:
+
+  document.querySelector('.hamburger').addEventListener('click', function(e) {
+    e.preventDefault();
+    toggleMenu()
+  });
+}
+*/
