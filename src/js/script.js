56e216d87935ab986e5c092e6147b2e487eb2bb5
Kasia Leśniewska <klesniewska29@gmail.com>
2020-06-16 19:31:56 +0200

    Add overlay and modal for exit icon in nav top

--- src/js/script.js
+++ src/js/script.js
@@ -21,3 +21,46 @@
   });
 }
 */
+
+/*
+{
+  function closeModal() {
+    document.getElementById('overlay').classList.remove('show');
+  }
+
+  document.querySelectorAll('#overlay .js--close-modal').forEach(function(btn) {
+    btn.addEventListener('click', function(e) {
+      e.preventDefault();
+      closeModal();
+    });
+  });
+
+  document.querySelector('#overlay').addEventListener('click', function(e) {
+    if(e.target === this) {
+      closeModal();
+    }
+  });
+
+  document.addEventListener('keyup', function(e) {
+    if(e.keyCode === 27) {
+      closeModal();
+    }
+  });
+
+  function openModal(modal) {
+    document.querySelectorAll('#overlay > *').forEach(function(modal) {
+      modal.classList.remove('show');
+    });
+    document.querySelector('#overlay').classList.add('show');
+    document.querySelector(modal).classList.add('show');
+  }
+
+  /*
+  Powyższy kod możemy wywołać, kiedy tylko będziemy potrzebować pokazać konkretny modal, np. openModal('#myModal').
+
+  const button = document.querySelector('#exitIcon');
+  button.addEventListener('click', function() {
+    openModal('#logOut');
+  });
+}
+*/
